cff6298b0fe0643a28d833707fa18937d49485f7
Vishalkaman <vishalkaman03@gmail.com>
2024-07-20 09:24:50 +0530

    Additions to UserRoutes
    Added getUserByID, getUserByUsername
    Modified loginUser to return user details as json

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,6 +25,40 @@ const getAllUsers = async (req, res) => {
     }
 };
 
+const getUserById = async (req, res) => {
+    const userId = req.params.id;
+    try {
+        const [user] = await db.execute('SELECT * FROM Users WHERE id = ?', [userId]);
+        if (user.length === 0) {
+            return res.status(404).json({ error: "User not found" });
+        }
+        res.status(200).json(user[0]);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const getUserByUsername = async (req, res) => {
+    const username = req.params.username;
+    try {
+        const [rows] = await db.execute('SELECT username, email, contactNumber FROM Users WHERE username = ?', [username]);
+        if (rows.length === 0) {
+            return res.status(404).json({ error: "User not found" });
+        }
+        const user = rows[0];
+        res.status(200).json({
+            username: user.username,
+            email: user.email,
+            contactNumber: user.contactNumber
+        });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+
+
+
 const loginUser = async (req, res) => {
     const { username, password } = req.body;
     try {
@@ -34,7 +68,12 @@ const loginUser = async (req, res) => {
             const match = await bcrypt.compare(password, user.passwd);
             if (match) {
                 const token = jwt.sign({ id: user.id }, 'your_jwt_secret');
-                res.json({ token });
+                res.json({
+                    // token,
+                    username: user.username,
+                    email: user.email,
+                    contactNumber: user.contactNumber
+                });
             } else {
                 res.status(400).json({ error: 'Invalid credentials' });
             }
@@ -46,9 +85,12 @@ const loginUser = async (req, res) => {
     }
 };
 
+
 module.exports = {
     createUser,
     getAllUsers,
-    loginUser
+    loginUser,
+    getUserById,
+    getUserByUsername
 };
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const { createUser, getAllUsers, loginUser } = require('../controllers/userController');
+const { createUser, getAllUsers, loginUser, getUserById, getUserByUsername} = require('../controllers/userController');
 
 const router = express.Router();
 
 router.post('/', createUser);
 router.get('/', getAllUsers);
 router.post('/login', loginUser);
+router.get('/id/:id', getUserById);
+router.get('/username/:username', getUserByUsername);
 
 module.exports = router;
 
